d594bf382c0cf381f26c1db5bf66f69e68067ad3
fattony26 <marco.moran26@hotmail.com>
2021-11-30 14:28:55 -0500

    Debug as True and Allowed Hosts empty for localhost runserver to keep app development going

--- workoutcity/settings.py
+++ workoutcity/settings.py
@@ -27,10 +27,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = []
+# '127.0.0.1', '.herokuapp.com'
 
 # Application definition
 
