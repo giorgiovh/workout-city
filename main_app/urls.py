7bbf46eeab4675193e6c1078fb73606f5176c466
fattony26 <marco.moran26@hotmail.com>
2021-11-30 18:47:35 -0500

    added workouts_detail function in views.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('workouts/', views.workouts_index, name='workouts_index'),
+    path('workouts/<int:workout_id>/', views.workouts_detail, name='workouts_detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -12,3 +12,7 @@ def about(request):
 def workouts_index(request):
     workouts = Workout.objects.all()
     return render(request, 'workouts/index.html', {'workouts': workouts})
+
+def workouts_detail(request, workout_id):
+    workout = Workout.objects.get(id=workout_id)
+    return render
