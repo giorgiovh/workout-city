f6359fcca0951da430ad3e35519b03832c6f37d9
smj2021 <mjschwartz515@gmail.com>
2021-11-30 16:15:23 -0500

    add hardcoded workouts to test

--- main_app/views.py
+++ main_app/views.py
@@ -11,5 +11,17 @@ def about(request):
     return render(request, 'about.html')
 
 
-class Workout: #This is the class & list function for creating workouts
+class Workout:  # This is the class & list function for creating workouts
     def __init__(self, muscle_grp, day_of_week, description, difficulty):
+        self.muscle_grp = muscle_grp
+        self.day_of_week = day_of_week
+        self.description = description
+        self.difficulty = difficulty
+
+
+workouts = [
+    Workout('Legs', 'Monday', '4x10 exercises', 4),
+    Workout('Chest', 'Tuesday', '5x10 exercises', 2),
+    Workout('Back', 'Monday', '5x15 exercises', 6),
+    Workout('Shoulders and Arms', 'Friday', '5x10 exercises', 5),
+]
