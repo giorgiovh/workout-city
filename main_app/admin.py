8383d7c3709098e711d705ecd6aa0a634fe01b95
fattony26 <marco.moran26@hotmail.com>
2021-11-30 18:40:24 -0500

    deleted workouts and class Workouts from views.py to remove the hardcode

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,5 @@
-from _typeshed import WriteableBuffer
 from django.contrib import admin
 from .models import Workout
-# Register your models here.
+
+# Register your models here
 admin.site.register(Workout)
