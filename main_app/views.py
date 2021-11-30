dcceeafc984d4822545addc52b4415856e41fc6e
smj2021 <mjschwartz515@gmail.com>
2021-11-30 16:10:49 -0500

    Workout view (controller action)

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+
+
 def home(request):
     return HttpResponse('<h1>Hello Planet</h1>')
 
+
 def about(request):
     return render(request, 'about.html')
+
+
+class Workout: #This is the class & list function for creating workouts
+    def __init__(self, muscle_grp, day_of_week, description, difficulty):
