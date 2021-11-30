29ff32fb7b38d4bc1d3817bbe95336f123079138
smj2021 <mjschwartz515@gmail.com>
2021-11-30 18:04:14 -0500

    add Workout model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Workout(models.Model):
+    muscle_grp = models.CharField(max_length=50)
+    day_of_week = models.CharField(max_length=50)
+    description = models.TextField(max_length=100)
