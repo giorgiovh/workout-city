46306ef10176192def5333ffb21513036c9ea87a
smj2021 <mjschwartz515@gmail.com>
2021-11-30 18:15:59 -0500

    add str method to Workout model

--- main_app/models.py
+++ main_app/models.py
@@ -7,3 +7,6 @@ class Workout(models.Model):
     muscle_grp = models.CharField(max_length=50)
     day_of_week = models.CharField(max_length=50)
     description = models.TextField(max_length=100)
+
+    def __str__(self):
+        return self.muscle_grp
