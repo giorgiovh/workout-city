77c3a85c6c647872cd94f6aa320441fbf49e7dfc
smj2021 <mjschwartz515@gmail.com>
2021-12-01 14:22:12 -0500

    add ordering to didworkout

--- main_app/models.py
+++ main_app/models.py
@@ -31,5 +31,8 @@ class DidWorkout(models.Model):
 
     def __str__(self):
         return f"{self.get_did_workout_display()} on {self.date}"
+    
+    class Meta:
+        ordering = ['-date']
         
     
