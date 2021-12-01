d1d39e94bf4223a5b8086ae6916395a00788b525
giorgiovh <jorgevonhoroch@gmail.com>
2021-12-01 10:23:12 -0500

    adds create workout path

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('workouts/', views.WorkoutList.as_view(), name='workouts_index'),
     path('workouts/<int:workout_id>/', views.workouts_detail, name='workouts_detail'),
+    path('workouts/create/', views.WorkoutCreate.as_view(), name='workouts_create'),
     path('accounts/signup/', views.signup, name='signup'),
 ]
