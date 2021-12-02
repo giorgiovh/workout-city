5a71e90de77d4861989d5629c1e6f366866fc04e
giorgiovh <jorgevonhoroch@gmail.com>
2021-12-02 15:02:16 -0500

    adds event listener to addworkoutdatebtn

---
+++ main_app/static/js/detail.js
@@ -0,0 +1,10 @@
+const progressBar = document.querySelector('.progress-bar')
+const addWorkoutDateBtn = document.querySelector('.btn.submit')
+console.log(progressBar);
+console.log(addWorkoutDateBtn);
+console.log('hello');
+
+
+addWorkoutDateBtn.addEventListener("click", (evt) => {
+    console.log('clicked');
+})
