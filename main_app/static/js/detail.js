3fc020154285d949a6fa41d20e19d2f25afe1078
giorgiovh <jorgevonhoroch@gmail.com>
2021-12-02 16:18:15 -0500

    adds progress bar message

--- main_app/static/js/detail.js
+++ main_app/static/js/detail.js
@@ -1,13 +1,19 @@
 const progressBar = document.querySelector('.progress-bar')
 const addWorkoutDateBtn = document.querySelector('.btn.submit')
 const didWorkoutInpt = document.querySelector('#id_did_workout')
+const progressMsgEl = document.querySelector('.progress-msg')
+
+console.log(progressMsgEl);
 
 let progressNumber = 0
 
 addWorkoutDateBtn.addEventListener("click", (evt) => {
     evt.preventDefault();
-    if (didWorkoutInpt.value === "Y") {        
-        progressNumber+= 25;
-        progressBar.setAttribute("style", `width: ${progressNumber}%`)
+    if (progressNumber < 100) {        
+        if (didWorkoutInpt.value === "Y") {        
+            progressNumber+= 25;
+            progressBar.setAttribute("style", `width: ${progressNumber}%`)
+            progressNumber === 100 ? progressMsgEl.innerText = "It's over 9000! You're one step closer to Super Saiyan status" : '';
+        }
     }
 })
