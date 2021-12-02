8b3d290f24bace16c55a040b03a519f296317425
giorgiovh <jorgevonhoroch@gmail.com>
2021-12-02 15:23:53 -0500

    increments progress number on click

--- main_app/static/js/detail.js
+++ main_app/static/js/detail.js
@@ -4,8 +4,11 @@ console.log(progressBar);
 console.log(addWorkoutDateBtn);
 console.log('hello');
 
+let progressNumber = 0
 
 addWorkoutDateBtn.addEventListener("click", (evt) => {
     evt.preventDefault();
+    progressNumber++;
+    console.log(progressNumber);
     progressBar.setAttribute("style", "width: 5%")
 })
